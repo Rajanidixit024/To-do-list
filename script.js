15194cd9651628232968a85bc9dd5c5aa37d5eee
Rajani Dixit <rajanidixit024@gmail.com>
2023-06-12 01:32:18 +0530

    first

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-let inputTag=document.getElementsByTagName("input")[0];
-let todo=document.getElementsByClassName("list")[0];
-//console.log(inputTag);
-function addnewtodo(){
-    if(inputTag.value.length<1) {
-        return;
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value=== ''){
+        alert("Add the task First...!!");
     }
-    let newTag=document.createElement("p");
-    newTag.textContent
-};
+    else{
+        let li= document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    saveData();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false)
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+function showList(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+showList();
